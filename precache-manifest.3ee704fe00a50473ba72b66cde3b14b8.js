61b978f23e6e95c24f11389c8ffea8f1d033385f
airmonsters <lai2009123@gmail.com>
2020-05-15 22:33:07 -0700

    Updates

---
+++ precache-manifest.3ee704fe00a50473ba72b66cde3b14b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d7c074b83d9ea5f735d2b65b66f68d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "778d2ec57b0d9a04157f",
+    "url": "./static/css/main.a644f2c9.chunk.css"
+  },
+  {
+    "revision": "cce870c53aea93e28f2f",
+    "url": "./static/js/2.d43570aa.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "./static/js/2.d43570aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778d2ec57b0d9a04157f",
+    "url": "./static/js/main.7275e6de.chunk.js"
+  },
+  {
+    "revision": "0d2373d6ed7feee294c2",
+    "url": "./static/js/runtime-main.9f834337.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "./static/media/image.d7265326.png"
+  }
+]);
